254f0d13ed447d4cc78da78fc339097c15565eeb
asadkarim <asadkarim33@gmail.com>
2021-05-08 08:14:12 -0700

    working on new project

---
+++ DigitalBookLibrary/Navigation/ScreenInit.js
@@ -0,0 +1,59 @@
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Home from '../Screens/home'
+import Download from '../Screens/Download';
+import SignIn from '../Screens/SignIn';
+import SignUp from '../Screens/SignUp';
+import Bookdetails from '../Screens/Bookdetails'
+
+
+
+
+
+const Stack = createStackNavigator();
+
+
+const screenOptionStyle = {
+  headerTransparent: false,
+  headerTitle: null,
+  headerLeft: null,
+};
+
+Stack.Navigator.defaultProps = {
+  headerMode: 'none',
+};
+
+
+function MyStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="Bookdetails" component={Bookdetails} />
+        <Stack.Screen name="Download" component={Download} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const SettingStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+    <Stack.Screen name="heartylunch" component={Heartylunch} />
+    <Stack.Screen name="weightLossTips" component={WeightLossTips} />
+    
+
+    </Stack.Navigator>
+  );
+}
+
+
+
+
+export default MyStack;
+
---
+++ DigitalBookLibrary/Screens/home.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import { Text,TextInput, View, Image, ScrollView,StyleSheet, TouchableOpacity } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { SliderBox } from "react-native-image-slider-box";
+
+
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [
+        "https://source.unsplash.com/1024x768/?nature",
+        "https://source.unsplash.com/1024x768/?water",
+        "https://source.unsplash.com/1024x768/?tree", // Network image
+                 // Local image
+      ]
+    };
+  }
+  render() {
+    return (
+      <View style = {{
+        flex:1
+      }}>
+
+<View style = {{
+        flex:2,
+        backgroundColor:"green"
+      }}>
+
+<View style = {{
+  flexDirection:'row',
+  justifyContent:'space-between',
+  alignSelf:'center',
+  alignItems:'center',
+  width:wp('95%')
+}}>
+  <TouchableOpacity>
+   <FontAwesome5 name="bars" size={30} color="white" />
+</TouchableOpacity>
+
+<View>
+  <Text style = {{
+    fontSize:20,
+    fontWeight:'800',
+    color:'white'
+  }}>Home</Text>
+</View>
+</View>
+
+<View  style = {{
+  height:70,
+  justifyContent:'center',
+}}>
+<TextInput placeholder={"Search books, Digest, Noval Dictionary"} style = {{
+  width:wp('94%'),
+  height:40,
+  paddingLeft:10,
+  alignSelf:'center',
+borderRadius:10,
+  backgroundColor:'white'
+}} />
+</View>
+      
+      </View>
+
+
+      <View style = {{
+        flex:3.5,
+        
+
+      }}>
+      
+      <SliderBox
+  images={this.state.images}
+  onCurrentImagePressed={index => console.warn(`image ${index} pressed`)}
+  currentImageEmitter={index => console.warn(`current pos is: ${index}`)}
+/>
+
+      </View>
+
+
+      <View style = {{
+        flex:7,
+        backgroundColor:"white"
+      }}>
+
+        <View style = {{
+          flexDirection:'row',
+          width:wp("98%"),
+          alignSelf:'center',
+          height:40,
+          alignItems:'center',
+          justifyContent:'space-between'
+        }}>
+          <Text style = {{
+          fontSize:20,
+          fontWeight:"400"
+          }}>Category</Text>
+          <TouchableOpacity>
+            <Text style = {{
+            color:'red'
+            }}>
+            ViewAll
+            </Text>
+            </TouchableOpacity>
+        </View>
+
+
+      
+      </View>
+      
+      </View>
+    );
+  }
+}
+
+export default Home;
