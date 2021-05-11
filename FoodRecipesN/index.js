982ac0253e264ed9a7bf86a10f7a2c240b62fb87
asadkarim <asadkarim33@gmail.com>
2021-05-11 04:32:38 -0700

    structural the project

--- FoodRecipesN/index.js
+++ FoodRecipesN/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './Screens/signin';
+import App from './Screens/AboutDish';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
