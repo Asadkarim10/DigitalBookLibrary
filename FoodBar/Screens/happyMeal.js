2912600750db6621da72e1ab77db58b57afd4e46
asadkarim <asadkarim33@gmail.com>
2021-05-15 05:09:58 -0700

    happymeals screen completed

--- FoodBar/Screens/happyMeal.js
+++ FoodBar/Screens/happyMeal.js
@@ -1,95 +1,240 @@
 import React from 'react';
 import { Text, TextInput, View, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+
+import SwiperComponent from './imgSwipper'
 const HappyMeal = () => {
-  return (
+    return (
+
+        <View style={{
+            flex: 1,
+            backgroundColor: 'white'
+        }}>
+
+            <View style={{
+                width: wp('98%'),
+                alignSelf: 'center',
+                backgroundColor: 'white',
+                flex: .5,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}>
+
+                <TouchableOpacity>
+                    <View>
+                        <FontAwesome5 name="bars" size={30} color="black" />
+                    </View>
+                </TouchableOpacity>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    marginRight: 8
+                }} >
+                    <View style={{ marginRight: 10 }}>
+                        <TouchableOpacity>
+                            <Ionicons name="search-sharp" size={30} color="black" />
+                        </TouchableOpacity>
+                    </View>
+
+                    <TouchableOpacity>
+                        <FontAwesome name="bell" size={30} color="black" />
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+
+
+            <View style={{
+                flex: 2,
+            }}>
+
+                <SwiperComponent />
+
+            </View>
+
+            <View style={{
+                flex: 4,
+                backgroundColor: "white"
+            }}>
+
+                <View style={{
+                    height: 90,
+                    marginTop: 10,
+                    width: wp('92%'),
+                    alignSelf: 'center',
+                    backgroundColor: 'white',
+                    flexDirection: 'row'    }}>
+
+                    <View style={{
+
+                    }}>
+
+                        <Image style={{ width: wp('18%'), height: 70, borderRadius: 5 }}
+                            source={require('../Assets/bf.jpg')} />
+                    </View>
+
+
+                    <View style={{
+                        width: wp('66%'),
+                        borderBottomWidth: 2,
+                        borderBottomColor: '#fcfcfc'
+                    }}>
+                        <View style={{
+                            marginLeft: 5,
+                            marginTop: 5,
+                        }}>
+                            <Text style={{
+                                color: '#717170',
+                                fontWeight: '700'
+                            }}>BreakFast</Text>
+                        </View>
+
+
+                        <View style={{
+                            marginTop: 2,
+                            marginLeft: 5
+                        }}>
+                            <Text style={{
+                                color: '#949494',
+                                fontWeight: '700'
+                            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</Text>
+                        </View>
+
+                    </View>
+                </View>
+
+
+
+
+                <View style={{
+                    height: 90,
+                    marginTop: 10,
+                    width: wp('92%'),
+                    alignSelf: 'center',
+                    backgroundColor: 'white',
+                    flexDirection: 'row'    }}>
+
+                    <View style={{
+
+                    }}>
+
+                        <Image style={{ width: wp('18%'), height: 70, borderRadius: 5 }}
+                            source={require('../Assets/bf.jpg')} />
+                    </View>
+
+
+                    <View style={{
+                        width: wp('66%'),
+                        borderBottomWidth: 2,
+                        borderBottomColor: '#fcfcfc'
+                    }}>
+                        <View style={{
+                            marginLeft: 5,
+                            marginTop: 5,
+                        }}>
+                            <Text style={{
+                                color: '#717170',
+                                fontWeight: '700'
+                            }}>All Time Favourite</Text>
+                        </View>
+
+
+                        <View style={{
+                            marginTop: 2,
+                            marginLeft: 5
+                        }}>
+                            <Text style={{
+                                color: '#949494',
+                                fontWeight: '700'
+                            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</Text>
+                        </View>
+
+                    </View>
+                </View>
+
+
+                <View style={{
+                    height: 90,
+                    marginTop: 10,
+                    width: wp('92%'),
+                    alignSelf: 'center',
+                    backgroundColor: 'white',
+                    flexDirection: 'row'    }}>
+
+                    <View style={{
+
+                    }}>
+
+                        <Image style={{ width: wp('18%'), height: 70, borderRadius: 5 }}
+                            source={require('../Assets/bf.jpg')} />
+                    </View>
+
+
+                    <View style={{
+                        width: wp('66%'),
+                        borderBottomWidth: 2,
+                        borderBottomColor: '#fcfcfc'
+                    }}>
+                        <View style={{
+                            marginLeft: 5,
+                            marginTop: 5,
+                        }}>
+                            <Text style={{
+                                color: '#717170',
+                                fontWeight: '700'
+                            }}>HappyMeal</Text>
+                        </View>
+
+
+                        <View style={{
+                            marginTop: 2,
+                            marginLeft: 5,
+                            height:'auto'
+                        }}>
+                            <Text style={{
+                                color: '#949494',
+                                fontWeight: '700'
+                            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                        </View>
+
+                    </View>
+                </View>
 
     <View style = {{
-        flex:1,
-        backgroundColor:'white'
+        height:100,
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'center'
     }}>
-
-   <View style = {{
-       flex:.5,
-       backgroundColor:'yellow'
-   }}>
-
-    <View>
-        <Text>Asad</Text>
-    </View>
-
-    <View >
-        <Text>Asad</Text>
-        <Text>Asad</Text>
-    </View>
-
-    </View>
-
-
-   <View style = {{
-       flex:2,
-       backgroundColor:'green'
-   }}>
-
-       <Text>Swipper</Text>
-
-   </View>
-
-   <View style = {{
-       flex:4,
-       backgroundColor:"white"
-   }}>
-
-       <View style = {{
-           height:90,
-           marginTop:10,
-           width:wp('98%'),
-           alignSelf:'center',
-           backgroundColor:'white',
-           flexDirection:'row',
-           borderWidth:1,
-
-       }}>
-
-           <View style = {{
-               
-           }}>
-               
-           <Image style = {{ width:wp('28%') , height:88}}
-            source={require('../Assets/bf.jpg')}/>
-           </View>
-
-
-           <View>
-            <View style = {{
-                marginLeft:5,
-                marginTop:5,
-            }}>
-                <Text>BreakFast</Text>
-            </View>
-
+        <TouchableOpacity style = {{
+            width:wp('30%'),
+            height:40,
+            borderRadius:18,
+            flexDirection:'row',
+            alignContent:'center',
+            alignItems:'center',
+            justifyContent:'center',
+            backgroundColor:'#5d8fd4'
+        }}>
             
-            <View style = {{
-                marginTop:10,
-                marginLeft:5
-            }}>
-                <Text>Lorem ipsum pipsum</Text>
-            </View>
-
-            </View>
-       </View>
-
-       
-
-   </View>
-
-
-
-
+        <MaterialIcons name="restaurant" size={25} color="white" />
+        <Text style = {{
+            color:'white',
+            fontSize:16,
+            marginLeft:5
+        }}>Menu</Text>
+        </TouchableOpacity>
     </View>
-  );
+
+            </View>
+        </View>
+    );
 }
 
 export default HappyMeal;
