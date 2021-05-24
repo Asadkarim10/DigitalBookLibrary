e50404cc5bc23b03cfe6cadd79f8010329b4a23f
asadkarim <asadkarim33@gmail.com>
2021-05-24 13:12:13 -0700

    updated files

---
+++ BalaTikka/screens/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text,TouchableOpacity,View } from 'react-native';
+
+const Signup = () => {
+  return (
+      <View>
+    <Text>Hello, I am your cat!</Text>
+    </View>
+  );
+}
+
+export default Signup;
