bf663a0e313b9e8e2ae344d7b187835cd0945ea3
asadkarim <asadkarim33@gmail.com>
2021-05-25 03:44:26 -0700

    working on Adress page

--- BalaTikka/screens/Adress.js
+++ BalaTikka/screens/Adress.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+
 
 const Address = () => {
   return (
-    <Text>Hello, I am your cat!</Text>
+    <View style={{ flex:1 }}>
+
+
+      <View style={{flex:2,backgroundColor:'yellow'}}> 
+
+      </View>
+      
+      <View style={{flex:.7,backgroundColor:'green'}}> 
+
+      </View>
+      
+      <View style={{flex:3,backgroundColor:'red'}}> 
+
+      </View>
+      
+      <View style={{flex:2,backgroundColor:'pink'}}> 
+
+      </View>
+      
+      <View style={{flex:.5,backgroundColor:'purple'}}> 
+
+      </View>
+
+
+
+
+
+
+
+
+
+
+    </View>
   );
 }
 
