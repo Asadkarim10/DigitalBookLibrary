c06e34bb409914362da05584d0d123358acd6fac
asadkarim <asadkarim33@gmail.com>
2021-05-25 12:58:02 -0700

    working on file

--- BalaTikka/screens/Adress.js
+++ BalaTikka/screens/Adress.js
@@ -6,18 +6,95 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 const Address = () => {
   return (
-    <View style={{ flex:1 }}>
+    <View style={{ flex:1, backgroundColor:"#f3f3f3" }}>
 
 
-      <View style={{flex:2,backgroundColor:'yellow'}}> 
+      <View style={{flex:2 , justifyContent:'center', flexDirection:'row', alignItems:'center'}}> 
+
+      <Image
+              style={{
+                  width: 150,
+                  height: 150
+              }}
+              source={require('../Assets/logo.png')}
+          />
 
       </View>
       
-      <View style={{flex:.7,backgroundColor:'green'}}> 
+      <View style={{flex:.7,backgroundColor:'green' , justifyContent:'center'}}> 
+
+<View style = {{
+  flexDirection:'row',
+  justifyContent:'space-around'
+}}>
+
+      <View style = {{
+        width:wp('30'),
+        alignItems:'center',
+        alignItems:'center'
+      }}>
+        <Text>Icons</Text>
+        <Text>@ballaTikkahouse</Text>
+      </View>
+
+      <View style={{
+    width: 1,
+    height:30,
+    backgroundColor: '#909090',}}>
+      
+    </View>
+
+    <View style = {{
+        width:wp('30'),
+        alignItems:'center',
+        alignItems:'center'
+      }}>
+        <Text>Icons</Text>
+        <Text>#ballaTikkahouse</Text>
+      </View>
+
+
+      <View style={{
+    width: 1,
+    height:30,
+    backgroundColor: '#909090',}}>
+      
+    </View>
+
+    <View style = {{
+        width:wp('30'),
+        alignItems:'center',
+        alignItems:'center'
+      }}>
+        <Text>Icons</Text>
+        <Text>ballaTikkahouse.com</Text>
+      </View>
+
+
+      </View>
+
+      
+      
+
+      
+
+
+
 
       </View>
       
-      <View style={{flex:3,backgroundColor:'red'}}> 
+      
+      <View style={{flex:3}}> 
+
+      <Image
+              style={{
+                  width: 120,
+                  height: 150
+              }}
+              source={require('../Assets/bala.png')}
+          />
+
+
 
       </View>
       
--- BalaTikka/screens/delivery.js
+++ BalaTikka/screens/delivery.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+
 
 const Delivery = () => {
   return (
-    <Text>Hello, I am your cat!</Text>
+    <View style = {{
+      flex:1,
+      backgroundColor:'white'
+    }}>    
+      <View style = {{
+        flex:.7,
+        backgroundColor:'green'
+      }}></View>
+      <View style = {{
+        flex:3,
+        backgroundColor:'yellow'
+      }}></View>
+      <View style = {{
+        flex:1,
+        backgroundColor:'blue'
+      }}></View>
+      <View  style = {{
+        flex:2,
+        backgroundColor:'green'
+      }}></View>
+      <View style = {{
+        flex:1,
+        backgroundColor:'red'
+      }}></View>
+
+    </View>
+
   );
 }
 
