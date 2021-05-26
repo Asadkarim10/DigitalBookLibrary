19d420de10258beaee7e9814ad1d68ffcfc8ffd6
asadkarim <asadkarim33@gmail.com>
2021-05-26 12:22:49 -0700

    front page done

--- BalaTikka/index.js
+++ BalaTikka/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './screens/signup' ;
+import App from './screens/home' ;
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
